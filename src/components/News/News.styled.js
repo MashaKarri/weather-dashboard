8606250300e346193ac7296e95b84164fa9012b6
Mariya Dovgalyuk <162305105+MashaKarri@users.noreply.github.com>
2026-06-29 12:11:51 +0300

    added adaptive to news

--- src/components/News/News.styled.js
+++ src/components/News/News.styled.js
@@ -4,6 +4,16 @@ export const Section = styled.section`
   max-width: 1140px;
   margin: 0 auto;
   padding: 80px 16px 18px;
+
+  @media (max-width: 1199px) {
+    max-width: 768px;
+    padding-top: 70px;
+  }
+
+  @media (max-width: 767px) {
+    max-width: 320px;
+    padding: 60px 0 18px;
+  }
 `;
 
 export const Title = styled.h2`
@@ -11,12 +21,27 @@ export const Title = styled.h2`
   font-family: 'Montserrat', sans-serif;
   font-size: 20px;
   font-weight: 500;
+
+  @media (max-width: 767px) {
+    margin-bottom: 25px;
+    text-align: center;
+  }
 `;
 
 export const Grid = styled.div`
   display: grid;
   grid-template-columns: repeat(4, 1fr);
   gap: 20px;
+
+  @media (max-width: 1199px) {
+    grid-template-columns: repeat(2, 1fr);
+    gap: 24px;
+  }
+
+  @media (max-width: 767px) {
+    grid-template-columns: 1fr;
+    gap: 30px;
+  }
 `;
 
 export const Card = styled.a`
@@ -37,6 +62,11 @@ export const Image = styled.div`
     object-fit: cover;
     display: block;
   }
+
+  @media (max-width: 767px) {
+    height: 180px;
+    margin-bottom: 15px;
+  }
 `;
 
 export const Text = styled.p`
@@ -45,10 +75,15 @@ export const Text = styled.p`
   font-size: 16px;
   line-height: 1.3;
   font-weight: 500;
+
+  @media (max-width: 767px) {
+    font-size: 15px;
+  }
 `;
 
 export const Button = styled.button`
-  margin-top: 20px;
+  display: block;
+  margin: 30px auto 0;
   width: 138px;
   height: 40px;
   border: none;
@@ -58,4 +93,9 @@ export const Button = styled.button`
   font-size: 16px;
   font-weight: 500;
   cursor: pointer;
+
+  @media (max-width: 767px) {
+    width: 100%;
+    max-width: 320px;
+  }
 `;
