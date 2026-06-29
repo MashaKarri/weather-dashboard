5eedeab49ee48241ea6880e11807620036f9efec
Mariya Dovgalyuk <162305105+MashaKarri@users.noreply.github.com>
2026-06-29 13:33:18 +0300

    added adaptive to details

--- src/components/WeatherDetails/WeatherDetails.styled.js
+++ src/components/WeatherDetails/WeatherDetails.styled.js
@@ -7,12 +7,34 @@ export const Container = styled.div`
   padding: 40px 77px;
   background: #e8e8e8;
   border-radius: 20px;
+  box-sizing: border-box;
+
+  @media (max-width: 1199px) {
+    max-width: 768px;
+    padding: 30px;
+    margin: 50px auto;
+  }
+
+  @media (max-width: 767px) {
+    max-width: 300px;
+    padding: 40px;
+    margin: 40px auto;
+  }
 `;
 
 export const Grid = styled.div`
   display: grid;
   grid-template-columns: repeat(3, 1fr);
   gap: 58px;
+
+  @media (max-width: 1199px) {
+    gap: 20px;
+  }
+
+  @media (max-width: 767px) {
+    grid-template-columns: 1fr;
+    gap: 35px;
+  }
 `;
 
 export const Item = styled.div`
@@ -25,18 +47,44 @@ export const Item = styled.div`
   justify-content: center;
   align-items: center;
   font-family: 'Montserrat', sans-serif;
+
+  @media (max-width: 1199px) {
+    height: 200px;
+  }
+
+  @media (max-width: 767px) {
+    height: 160px;
+    padding: 15px;
+  }
 `;
 
 export const Title = styled.p`
   margin: 0;
   font-size: 16px;
   font-weight: 500;
+
+  @media (max-width: 1199px) {
+    font-size: 12px;
+  }
+
+  @media (max-width: 767px) {
+    font-size: 10px;
+  }
 `;
 
 export const Value = styled.p`
   margin: 10px 0;
   font-size: 32px;
   font-weight: 500;
+
+  @media (max-width: 1199px) {
+    font-size: 16px;
+  }
+
+  @media (max-width: 767px) {
+    font-size: 14px;
+    margin: 8px 0;
+  }
 `;
 
 export const Icon = styled.div`
@@ -51,5 +99,17 @@ export const Icon = styled.div`
     width: 100%;
     height: 100%;
     object-fit: contain;
+  }
+
+  @media (max-width: 1199px) {
+    width: 60px;
+    height: 60px;
+    margin-top: 37px;
+  }
+
+  @media (max-width: 767px) {
+    width: 60px;
+    height: 60px;
+    margin-top: 18px;
   }
 `;
